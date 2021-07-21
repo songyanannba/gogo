fa1919eb0c85e06c602517b7df7c716f702b13e4
songyanan <songyanan@xinxinapp.cn>
2021-07-21 20:21:29 +0800

    go 算法练习

--- go_syn/webApp/utils/db.go
+++ go_syn/webApp/utils/db.go
@@ -12,7 +12,7 @@ var (
 )
 
 func init() {
-	Db, err = sql.Open("mysql", "root:003416nba@tcp(localhost:3306)/test")
+	Db, err = sql.Open("mysql", "root:密码@tcp(localhost:3306)/test")
 	if err != nil {
 		panic(err.Error())
 	}
